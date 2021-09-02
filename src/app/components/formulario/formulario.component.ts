8f96fb3bc6d991d8918279990db356fa805c3545
9L5KE <luisenriquefloresf@gmail.com>
2021-09-02 13:15:57 -0500

    3 files modified

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
-  constructor() {}
+  constructor() {
+    this.selector_input = ['', 'uno', 'dos', 'tres'];
+    this.texto_input = '';
+  }
+  public verifica() {
+  /*alert(
+      'verificando ' +
+        this.numerico_input +
+        '||' +
+        this.texto_input +
+        ' || ' +
+        this.seleccionado_input
+    );*/
+    if (
+      this.numerico_input != null &&
+      this.texto_input != '' &&
+      (this.seleccionado_input != undefined || this.seleccionado_input != '')
+    )
+      this.botonDeshabilitado = false;
+    else this.botonDeshabilitado = true;
+  }
 
   ngOnInit(): void {}
+  public texto_input: string;
+  public numerico_input: number;
+  public comentario_input: string;
+  public selector_input: string[];
+  public seleccionado_input: string;
+  public botonDeshabilitado = true;
 }
