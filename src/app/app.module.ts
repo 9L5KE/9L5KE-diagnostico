206a2086d42830ac023ec37ca85a79301254106b
9L5KE <luisenriquefloresf@gmail.com>
2021-09-02 09:21:30 -0500

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-
+import { FormularioComponent } from './components/formulario/formulario.component';
+const routes: Routes = [
+  //vistas
+  {
+    path: 'formulario',
+    component: FormularioComponent
+  }
+];
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, FormularioComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
